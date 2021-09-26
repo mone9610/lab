d5ed29a7913c278ed87250158efe4c7fcdbf6243
mone9610 <knemoto.prod@gmail.com>
2021-09-26 15:16:01 +0900

    002の記事を編集

---
+++ 002_react_mui_comment_list_mock/test/src/data.ts
@@ -0,0 +1,25 @@
+export const postData = [
+  {
+    id: 1,
+    user_id: 2,
+    message: "１コメゲットです。",
+    created_at: "2021-09-21T01:42:13.094Z",
+  },
+  {
+    id: 2,
+    user_id: 1,
+    message: "惜しくも２コメとなりました。",
+    created_at: "2021-09-22T01:42:13.094Z",
+  },
+];
+
+export const userData = [
+  {
+    id: 1,
+    name: "山田太郎",
+  },
+  {
+    id: 2,
+    name: "花子山田",
+  },
+];
